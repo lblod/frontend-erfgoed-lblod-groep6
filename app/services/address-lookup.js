2b7465ccfb18a35c6894e209a6ad438f810480ba
Joren Saey <joren.saey@codifly.be>
2024-09-12 14:33:00 +0200

    Implement service

--- app/services/address-lookup.js
+++ app/services/address-lookup.js
@@ -1,11 +1,27 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import fetch from 'fetch';
+import fetch from 'ember-fetch';
+
+const BASE_URL = 'localhost/adres-search';
 
 export default class AddressLookupService extends Service {
   @tracked addresses = [];
+  @tracked error = null;
+  @tracked loading = false;
 
-  lookup(searchTerm) {
-    fetch(`?searchTerm=${searchTerm}`, {});
+  async lookup(searchTerm) {
+    try {
+      this.error = null;
+      this.loading = true;
+      const response = await fetch(`${BASE_URL}/search?query="${searchTerm}"`, {
+        method: 'GET',
+      });
+      const data = await response.json();
+      this.addresses = data.adressen;
+    } catch (error) {
+      this.error = error;
+    } finally {
+      this.loading = false;
+    }
   }
 }
